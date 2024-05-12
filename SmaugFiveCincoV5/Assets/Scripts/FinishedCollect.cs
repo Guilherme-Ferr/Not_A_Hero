e58427aec1e2bb6853d75a35bb4010636d927f2d
Guilherme Ferreira da Silva <67923079+Guilherme-Ferr@users.noreply.github.com>
2024-05-12 12:50:04 -0300

    feat: Implementa mudança de cena após conclusão da coleta de vegetais

--- SmaugFiveCincoV5/Assets/Scripts/FinishedCollect.cs
+++ SmaugFiveCincoV5/Assets/Scripts/FinishedCollect.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishedCollect : MonoBehaviour
 {
@@ -11,6 +12,7 @@ public class FinishedCollect : MonoBehaviour
         if (collider.gameObject.CompareTag("Player") && collectVegetable.finishedCollecting)
         {
             Debug.Log("pode al mossa");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
 }
