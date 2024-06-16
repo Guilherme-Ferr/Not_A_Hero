df4613e08d57a978d3f83445c7de5d3cf3826356
Guilherme Ferreira da Silva <67923079+Guilherme-Ferr@users.noreply.github.com>
2024-06-15 23:02:33 -0300

    feat: Adiciona cena seguinte do heroi

--- SmaugFiveCincoV5/Assets/Scripts/NextCutscene.cs
+++ SmaugFiveCincoV5/Assets/Scripts/NextCutscene.cs
@@ -5,6 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class NextCutscene : MonoBehaviour
 {
+    private Animator anim;
+
+    void Start()
+    {
+        Debug.Log(SceneManager.GetActiveScene().buildIndex);
+        if (SceneManager.GetActiveScene().buildIndex == 2)
+        {
+            anim = GetComponent<Animator>();
+            anim.Play("CeneTwo");
+        }
+    }
+
     public void GoToPrison()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
