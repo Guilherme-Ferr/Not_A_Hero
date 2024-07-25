b7b3a90c3bc740ed49dcc84b0c9fe468331edf9d
Guilherme Ferreira da Silva <67923079+Guilherme-Ferr@users.noreply.github.com>
2024-07-25 00:39:54 -0300

    feat: Adiciona script para pausa mid-game

---
+++ SmaugFiveCincoV5/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenuUI;  // Referência ao menu de pausa
+    private bool isPaused = false;  // Flag para verificar se o jogo está pausado
+
+    void Update()
+    {
+        // Verifica se a tecla ESC foi pressionada
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);  // Esconde o menu de pausa
+        Time.timeScale = 1f;           // Retoma o tempo do jogo
+        isPaused = false;              // Atualiza o estado de pausa
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);   // Mostra o menu de pausa
+        Time.timeScale = 0f;           // Pausa o tempo do jogo
+        isPaused = true;               // Atualiza o estado de pausa
+    }
+}
